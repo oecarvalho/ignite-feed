41e9deb321efc46613a98f4a6ea8d16dae374f05
Felipe <felipe.carvalho.dm@gmail.com>
2025-08-09 19:19:47 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { Header } from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      <Header/>
+
+      <div className="whapper">
+        
+      </div>
+    </>
+  )
+}
+
+export default App
