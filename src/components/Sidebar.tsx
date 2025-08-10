ce76f9832cb7884f1dc1b99e24071f30d67f4c00
Felipe <felipe.carvalho.dm@gmail.com>
2025-08-10 01:34:46 -0300

    finalizando sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import {PencilLine} from 'phosphor-react'
+
 import styles from './Sidebar.module.css'
 
 export function Sidebar(){
@@ -6,12 +8,14 @@ export function Sidebar(){
             <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?q=40&w=500&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" />
 
             <div className={styles.profile}>
+                <img className='avatar' src="https://github.com/oecarvalho.png" alt="" />
+
                 <strong>Felipe Carvalho</strong>
                 <span>Web Developer</span>
             </div>
 
             <footer>
-                <a href="#">Editar o seu Perfil</a>
+                <a href="#"> <PencilLine size={20}/> Editar seu Perfil</a>
             </footer>
         </aside>
     )
